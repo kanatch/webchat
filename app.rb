14281426d701a2bdbc4a4e528833c6b58e781085
kanatch <kanattitti@gmail.com>
2022-12-11 05:38:09 +0000

    :tada:

---
+++ app.rb
@@ -0,0 +1,68 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require 'json'
+require 'open-uri'
+require 'net/http'
+
+require './models'
+
+enable :sessions
+
+helpers do
+  def current_user
+    User.find_by(id: session[:user])
+  end
+end
+
+get '/' do
+# 　@comments = Comment.order('id desc')
+  @comments = Comment.all
+  erb :index
+end
+
+get '/signin' do
+  erb :sign_in
+end
+
+get '/signup' do
+  erb :sign_up
+end
+
+post '/signin' do
+    user = User.find_by(name: params[:name])
+    if user && user.authenticate(params[:password])
+        session[:user] = user.id
+    end
+    redirect '/'
+end
+
+post '/signup' do
+    @user = User.create(name:params[:name], password:params[:password],
+    password_confirmation:params[:password_confirmation])
+    if @user.persisted?
+        session[:user] = @user.id
+    end
+    redirect '/'
+end
+
+get '/signout' do
+    session[:user] = nil
+    redirect '/'
+end
+
+
+post '/comment' do
+
+  Comment.create({
+    user_id: session[:user],
+    body: params[:body],
+    user_name: params[:user_id]
+  })
+end
+
+get '/comments/last' do
+  puts '11111111111111111111111111111111'
+  last_id = params[:last_id]
+  @comment = Comment.where('id > ?', last_id).to_json
+end
